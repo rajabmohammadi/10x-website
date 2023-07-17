7cd5759f4cdd3f7da9e6012f751a7c5c280d1fce
js_rajab <r.mohammadi@webfume.com>
2023-07-17 16:09:16 +0430

    fix the error

--- src/resolvers/paypal.resolver.ts
+++ src/resolvers/paypal.resolver.ts
@@ -8,49 +8,49 @@ let storeItems = new Map([
 const resolvers = {
     Query: {},
     Mutation: {
-        createOrder: async (_, args) => {
-            const request = new paypal.orders.OrdersCreateRequest();
-            const total = args.body.items.reduce((sum, item) => {
-                return sum + storeItems.get(item.id).price;
-            })
-            request.prefer("return=representation");
-            request.requestBody({
-                intent: "CAPTURE",
-                purchase_units: [
-                    {
-                        amount: {
-                            currency_code: "USD",
-                            value: total,
-                            breakdown: {
-                                item_total: {
-                                    currency_code: "USD",
-                                    value: total
-                                }
-                            }
-                        },
-                        items: args.body.items.map((item) => {
-                            const storeItem = storeItems.get(item.id);
-                            return {
-                                name: storeItem.name,
-                                unit_amount: {
-                                    currency_code: "USD",
-                                    value: storeItem.price
-                                }
-                            }
-                        })
-                    }
-                ]
-            })
+        // createOrder: async (_, args) => {
+        //     const request = new paypal.orders.OrdersCreateRequest();
+        //     const total = args.body.items.reduce((sum, item) => {
+        //         return sum + storeItems.get(item.id).price;
+        //     })
+        //     request.prefer("return=representation");
+        //     request.requestBody({
+        //         intent: "CAPTURE",
+        //         purchase_units: [
+        //             {
+        //                 amount: {
+        //                     currency_code: "USD",
+        //                     value: total,
+        //                     breakdown: {
+        //                         item_total: {
+        //                             currency_code: "USD",
+        //                             value: total
+        //                         }
+        //                     }
+        //                 },
+        //                 items: args.body.items.map((item) => {
+        //                     const storeItem = storeItems.get(item.id);
+        //                     return {
+        //                         name: storeItem.name,
+        //                         unit_amount: {
+        //                             currency_code: "USD",
+        //                             value: storeItem.price
+        //                         }
+        //                     }
+        //                 })
+        //             }
+        //         ]
+        //     })
 
-            try {
-                let response = await client.execute(request);
-                console.log(`Response: ${JSON.stringify(response)}`);
-                // If call returns body in response, you can get the deserialized version from the result attribute of the response.
-                console.log(`Order: ${JSON.stringify(response.result)}`);
-            } catch (err) {
+        //     try {
+        //         let response = await client.execute(request);
+        //         console.log(`Response: ${JSON.stringify(response)}`);
+        //         // If call returns body in response, you can get the deserialized version from the result attribute of the response.
+        //         console.log(`Order: ${JSON.stringify(response.result)}`);
+        //     } catch (err) {
 
-            }
-        }
+        //     }
+        // }
     }
 
 };
